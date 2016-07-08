abf06504df563cafd61fd69a210558507d52b5ab
Tony Pitale <tpitale@gmail.com>
2016-07-08 09:57:50 -0500

    Remove config for assets; remove config for rspec symbol keys

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ require File.expand_path('../fixtures/dummy/config/application.rb', __FILE__)
 RSpec.configure do |config|
   config.mock_with :mocha
 
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
